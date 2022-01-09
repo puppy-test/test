69e2d57e5004a4576ef5d7e594de0d2b7d33bf11
User <lenice@nate.com>
2022-01-09 23:02:34 +0900

    zz

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,9 +1,13 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
 
     void test1() {
         System.out.println("test 1");
+        Map<String,String> map = new HashMap<>();
+
+
     }
 
     void test2() {
