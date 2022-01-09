125444c278122bd7e4a9ab6f4b6edfd6dc77d37b
User <lenice@nate.com>
2022-01-09 21:35:31 +0900

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,11 +4,9 @@ public class Test {
 
     void test() {
         System.out.println("test 1");
+    }
+
+    void test2() {
         System.out.println("test 2");
-        System.out.println("test 3");
-        System.out.println("test 4");
-        System.out.println("test 5");
-        System.out.println("test 6");
-        System.out.println("test 7");
     }
 }
